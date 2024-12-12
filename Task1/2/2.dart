da09895ff94a6705a8845b0aa960294872b049a1
shams <shamsalalfy2005@gmail.com>
2024-12-13 00:40:07 +0200

    Task1

---
+++ Task1/2/2.dart
@@ -0,0 +1,7 @@
+void main()
+{
+  int distance = 25;
+  int speed = 40;
+  double time = distance / speed;
+  print("Time is $time hour");
+}
---
+++ Task1/3/3.dart
@@ -0,0 +1,25 @@
+void main()
+{
+  int t = 10;
+  print(t);
+  t--; // 10 -> 9
+  print(t);
+  t--; // 9 -> 8
+  print(t);
+  t--; // 8 -> 7
+  print(t);
+  t--; // 7 -> 6
+  print(t);
+  t--; // 6 -> 5
+  print(t);
+  t--; // 5 -> 4
+  print(t);
+  t--; // 4 -> 3
+  print(t);
+  t--; // 3 -> 2
+  print(t);
+  t--; // 2 -> 1
+  print(t);
+  t--; // 1 -> 0
+  print(t);
+}
