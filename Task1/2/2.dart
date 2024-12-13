8017e86975939bba1e0f322f35b41a51f9435301
shams <shamsalalfy2005@gmail.com>
2024-12-13 02:11:41 +0200

    Task1

--- Task1/2/2.dart
+++ Task1/2/2.dart
@@ -4,4 +4,8 @@ void main()
   int speed = 40;
   double time = distance / speed;
   print("Time is $time hour");
+  time *= 60;
+  print("Time is $time minutes");
+  time *= 60;
+  print("Time is $time seconds");
 }
